afcc7a085651b34d7d6cb3ac838111d633fa90c6
PGabriel20 <bielporreca@gmail.com>
2021-03-13 21:16:20 -0300

    Featt: added Feed component

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Feed from '../Feed';
+
 import { 
   Container,
   Banner,
@@ -7,7 +9,8 @@ import {
   ProfileData,
   LocationIcon,
   CakeIcon,
-  Followage 
+  Followage,
+  EditButton,
 } from './styles';
 
 const ProfilePage: React.FC = () => {
@@ -18,7 +21,7 @@ const ProfilePage: React.FC = () => {
       </Banner>
 
       <ProfileData>
-        {/* <EditButton outlined>Editar perfil</EditButton> */}
+        <EditButton outlined>Editar perfil</EditButton>
 
         <h1>Paulo Gabriel</h1>
         <h2>@bielllp</h2>
@@ -47,6 +50,8 @@ const ProfilePage: React.FC = () => {
           </span>
         </Followage>
       </ProfileData>
+
+      <Feed />
     </Container>
   );
 }
--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 
 import { LocationOn, Cake,  } from '../../styles/Icons'
+import Button from '../Button';
 
 export const Container = styled.div`
   display: flex;
@@ -113,5 +114,20 @@ export const Followage = styled.div`
     & + span {
       margin-left: 20px;
     }
+  }
+`;
+
+export const EditButton = styled(Button)`
+  position: absolute;
+  top: 2vw;
+  right: 7px;
+
+  padding: 4px 16px;
+  font-size: 13px;
+
+  @media(min-width: 320px){
+    top: 10px;
+    padding: 10px 19px;
+    font-size: 15px;
   }
 `;
