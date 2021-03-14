14b05e1c3d68c5b9f23b303df983cb9f5a0ce0bb
PGabriel20 <bielporreca@gmail.com>
2021-03-13 21:51:17 -0300

    Feat: added tweet component

---
+++ src/components/Tweet/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import {
+    Container,
+    Retweeted,
+    RocketseatIcon,
+    Body,
+    Avatar,
+    Content,
+    Header,
+    Dot,
+    Description,
+    ImageContent,
+    Icons,
+    Status,
+    CommentIcon,
+    RetweetIcon,
+    LikeIcon,
+  } from './styles';
+
+const Tweet: React.FC = () => {
+  return (
+    <Container>
+      <Retweeted>
+        <RocketseatIcon />
+        Você retweetou
+      </Retweeted>
+
+      <Body>
+        <Avatar />
+
+        <Content>
+          <Header>
+            <strong>Rocketseat</strong>
+            <span>@rockeseat</span>
+            <Dot />
+            <time>13 de mar</time>
+          </Header>
+
+          <Description>Foguete não tem ré</Description>
+
+          <ImageContent />
+
+          <Icons>
+            <Status>
+              <CommentIcon />
+              18
+            </Status>
+            <Status>
+              <RetweetIcon />
+              18
+            </Status>
+            <Status>
+              <LikeIcon />
+              999
+            </Status>
+          </Icons>
+        </Content>
+      </Body>
+    </Container>
+  );
+}
+
+export default Tweet;
